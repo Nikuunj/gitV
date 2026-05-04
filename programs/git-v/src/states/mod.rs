c50c5f5b52929107e093ed66b823c5d6219886b4
Nikunj <mnikunj2622@gmail.com>
2026-05-04 10:04:01 +0530

    update: state remove extra account

--- programs/git-v/src/states/mod.rs
+++ programs/git-v/src/states/mod.rs
@@ -1,14 +1,18 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct ConfigIssue {
     pub bump: u8,
     pub is_open: bool,
     pub issue_number: u32,
-    pub amount: u64,
     pub end_time: i64,
-    pub owner: Pubkey,
     pub created_at: i64,
+    pub reward_amount: u64,
+    pub owner: Pubkey,
+    pub reward_mint: Pubkey,
+    #[max_len(100)]
     pub repo_name: Vec<u8>,
+    #[max_len(50)]
     pub username: Vec<u8>,
 }
