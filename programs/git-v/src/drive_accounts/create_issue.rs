a5a919996421cf615bf12d4612a57fba30caa94a
Nikunj <mnikunj2622@gmail.com>
2026-05-04 10:05:34 +0530

    impl: create_issue and deposite fund derive account state

--- programs/git-v/src/drive_accounts/create_issue.rs
+++ programs/git-v/src/drive_accounts/create_issue.rs
@@ -1,6 +1,46 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{Mint, TokenAccount, TokenInterface}};
 
-use crate::drive_accounts;
+use crate::{drive_accounts, ConfigIssue};
+
+#[derive(Accounts)]
+#[instruction(username: Vec<u8>, reponame: Vec<u8>)]
+pub struct CreateIssue<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(mint::token_program = token_program)]
+    pub mint: InterfaceAccount<'info, Mint>,
 
 
-#[drive(Accounts)]
+    #[account(
+        init,
+        payer = owner,
+        space = ConfigIssue::INIT_SPACE + ConfigIssue::DISCRIMINATOR.len(),
+        seeds = [b"config", owner.key().as_ref(), &username, &reponame],
+        bump
+    )]
+    pub config_account: Account<'info, ConfigIssue>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = owner,
+        associated_token::token_program = token_program
+        
+    )]
+    pub owner_ata: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = owner,
+        associated_token::mint = mint,
+        associated_token::authority = config_account,
+        associated_token::token_program = token_program
+        
+    )]
+    pub issue_ata: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
--- programs/git-v/src/lib.rs
+++ programs/git-v/src/lib.rs
@@ -1,8 +1,4 @@
 use anchor_lang::prelude::*;
-use rust_decimal::Decimal;
-use switchboard_on_demand::{
-    on_demand::accounts::pull_feed::PullFeedAccountData, prelude::rust_decimal,
-};
 
 declare_id!("Sg4bheXXXdMrcTmsSr3d2bQZhmGmBeFJCJYtkXst3Hp");
 
@@ -16,33 +12,4 @@ pub use states::*;
 pub mod git_v {
 
     use super::*;
-
-    pub fn reed_feed(ctx: Context<ReadPrice>) -> Result<()> {
-        let data_slice = ctx.accounts.feed.data.borrow();
-
-        let feed = PullFeedAccountData::parse(data_slice).unwrap();
-
-        let price: Decimal = feed
-            .get_value(
-                Clock::get()?.slot,
-                /*max_stale_slots=*/ 100,
-                /*min_samples=*/ 3,
-                /*only_positive=*/ true,
-            )
-            .unwrap();
-
-        msg!("SOL/USD price: {}", price);
-
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct ConsumeDataAccounts {}
-
-#[derive(Accounts)]
-pub struct ReadPrice<'info> {
-    /// CHECK: This is a Switchboard pull feed account, validated by parsing its data
-    #[account(mut)]
-    pub feed: AccountInfo<'info>,
 }
