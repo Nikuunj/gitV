0f9d605981dc8bd3a190f3a56666cae8bf7e4987
Nikunj <mnikunj2622@gmail.com>
2026-05-04 18:23:50 +0530

    impl: module update

--- programs/git-v/src/drive_accounts/mod.rs
+++ programs/git-v/src/drive_accounts/mod.rs
@@ -1,3 +1,5 @@
+pub mod close_issue;
 pub mod create_issue;
 
+pub use close_issue::*;
 pub use create_issue::*;
--- programs/git-v/src/lib.rs
+++ programs/git-v/src/lib.rs
@@ -3,10 +3,12 @@ use anchor_lang::prelude::*;
 declare_id!("Sg4bheXXXdMrcTmsSr3d2bQZhmGmBeFJCJYtkXst3Hp");
 
 pub mod drive_accounts;
+pub mod errors;
 pub mod instructions;
 pub mod states;
 
 pub use drive_accounts::*;
+pub use errors::*;
 pub use instructions::*;
 pub use states::*;
 
